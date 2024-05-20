2de4044db02d506011fb7c91474a7faf108e57eb
HHHBBBRRR <3543977024@qq.com>
2024-05-20 17:01:05 +0800

    ✨ feat: add TokenType

--- globals.h
+++ globals.h
@@ -7,6 +7,16 @@
 #include <string.h>
 #include <stdbool.h>
 
+typedef enum {
+    /* Control */
+    ENDFILE, ERROR,
+    /* Keywords */
+    IF, THEN, ELSE ,END, REPEAT, UNTIL, READ, WRITE,
+    /*Identifier and Number */
+    ID, NUM,
+    /* Special symbols */
+    ASSIGN, EQ, LT, PLUS, MINUS, TIMES, OVER, LPAREN, RPAREN, SEMI
+} TokenType;
 
 
 #endif
