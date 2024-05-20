e802806114782c9c77d16e9516395a9d3fd0f64c
HHHBBBRRR <3543977024@qq.com>
2024-05-20 18:53:40 +0800

    ✨ feat: add printToken

--- util.h
+++ util.h
@@ -1,4 +1,11 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include "globals.h"
+
+/* Procedure printToken prints a token 
+ * and its lexeme to the listing file
+ */
+void printToken(TokenType token, const char *tokenString);
+
 #endif
