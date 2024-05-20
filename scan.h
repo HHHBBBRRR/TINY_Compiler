7552b5ab6b8afe9835957baa5360599be1c7440f
HHHBBBRRR <3543977024@qq.com>
2024-05-20 17:51:47 +0800

    ✨ feat: update scan.h

--- scan.h
+++ scan.h
@@ -3,6 +3,10 @@
 
 #include "globals.h"
 
+#define MAXTOKENLEN 40  // max length of token string
 
+extern char tokenString[MAXTOKENLEN + 1];   // holds token string(add '\0' in the end)
+
+TokenType getToken(void);
 
 #endif
