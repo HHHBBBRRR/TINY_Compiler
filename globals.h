ce97aa4f3cd5936d12459ddc18df3da79b733eed
HHHBBBRRR <3543977024@qq.com>
2024-05-21 18:21:58 +0800

    ✨ feat: add TreeNode

--- globals.h
+++ globals.h
@@ -7,21 +7,88 @@
 #include <string.h>
 #include <stdbool.h>
 
-typedef enum {
-    ENDFILE,  // when get to the end of file
-    ERROR,    // find something wrong
+typedef enum
+{
+    ENDFILE, // when get to the end of file
+    ERROR,   // find something wrong
     /* Keywords */
-    IF, THEN, ELSE ,END, REPEAT, UNTIL, READ, WRITE,
+    IF,
+    THEN,
+    ELSE,
+    END,
+    REPEAT,
+    UNTIL,
+    READ,
+    WRITE,
     /*Identifier and Number (need multi-char)*/
-    ID, NUM,
+    ID,
+    NUM,
     /* Special symbols */
-    ASSIGN, EQ, LT, PLUS, MINUS, TIMES, OVER, LPAREN, RPAREN, SEMI
+    ASSIGN,
+    EQ,
+    LT,
+    PLUS,
+    MINUS,
+    TIMES,
+    OVER,
+    LPAREN,
+    RPAREN,
+    SEMI
 } TokenType;
 
-extern FILE* source;    /* source code text file */
-extern FILE* listing;   /* listing output text file */
+extern FILE *source;  /* source code text file */
+extern FILE *listing; /* listing output text file */
 
 extern int lineno; /* source line number for listing */
+
+typedef enum
+{
+    StmtK,
+    ExpK
+} NodeKind;
+typedef enum
+{
+    IfK,
+    RepeatK,
+    AssignK,
+    ReadK,
+    WriteK
+} StmtKind;
+typedef enum
+{
+    OpK,
+    ConstK,
+    IdK
+} ExpKind;
+
+typedef enum
+{
+    Void,
+    Integer,
+    Boolean
+} ExpType;
+
+#define MAXCHILDREN 3 // if else end
+
+typedef struct treeNode
+{
+    struct treeNode *child[MAXCHILDREN];
+    struct treeNode *sibling;
+    int lineno;
+    NodeKind nodekind;
+    union
+    {
+        StmtKind stmt;
+        ExpKind exp;
+    } kind;
+    union
+    {
+        TokenType op;
+        int val;
+        char *name;
+    } attr;
+    ExpType type; /* for type checking of exps */
+} TreeNode;
 
 /**************************************************/
 /***********   Flags for tracing       ************/
